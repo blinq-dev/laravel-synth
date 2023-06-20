2cc13a93c07012daa001affe3556e7698cdfbb8f
Lennard <168357+lennardv2@users.noreply.github.com>
2023-06-20 13:40:40 +0200

    Improvements in modules

--- config/synth.php
+++ config/synth.php
@@ -2,5 +2,14 @@
 
 // config for Blinq/Synth
 return [
-
+    // OpenAI api key
+    'openai_key' => env('OPENAI_KEY'),
+    // Small model is used by default
+    'small_model' => 'gpt-3.5-turbo-0613',
+    // Large model is switched to when it receives a content length error message from OpenAI
+    'large_model' => 'gpt-3.5-turbo-16k-0613',
+    // The base path to search from
+    'file_base' => base_path(),
+    // Keep performance in mind when setting this value
+    'search_limit' => 10,
 ];
